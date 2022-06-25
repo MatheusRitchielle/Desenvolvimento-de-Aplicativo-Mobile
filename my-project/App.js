b7fd16a4e5f3ef8b42df7ddeae4d6bc04ce7fd80
MatheusRitchielle <matheusritchielle98@gmail.com>
2022-06-24 22:04:59 -0300

    Tela login

--- my-project/App.js
+++ my-project/App.js
@@ -1,29 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
-import Login from './login';
 
-export default function App() {
+import React from "react";
+import { View } from "react-native";
+import { ButtonText, Container, Login, PressableLogin, Title, InnerText } from "./styles";
+
+const Test = () => {
   return (
-    <View style={styles.container}>
-      <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-        <View style={{alignItems: 'center',  height: 25, width: 25, backgroundColor: "grey", borderRadius: 100}}>
-          <Text style={{fontWeight: 'bold'}}>X</Text>
+    <Container>
+      <Login>
+        <Title>
+          Login
+        </Title>
+        <View style={{ marginTop: 20 }}>
+          <InnerText placeholder="username"  />
+          <InnerText placeholder="password"  />
+          <PressableLogin>
+            <ButtonText>
+              Login
+            </ButtonText>
+          </PressableLogin>
         </View>
-        <View style={{ height: 100, width: 100, backgroundColor: "blue" }} />
-      </View>
-      <Login />
-    
-  
-      <StatusBar style="auto" />
-    </View>
+      </Login>
+    </Container>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    backgroundColor: '#fff',
-    justifyContent: 'space-between',
-  },
-});
+export default Test;
